3d1deb0360b74a4f0f3dde8ebc215c0ceb608e70
fffbot <38205055+fffbot@users.noreply.github.com>
2018-07-06 16:46:54 +0200

    Extract method

--- fff.py
+++ fff.py
@@ -1,14 +1,20 @@
 import praw
 import requests
 
+
+def process_submission(submission):
+    print("Encountered submission: " + submission.title + "; " + submission.url)
+    if 'factorio.com/blog/post/fff' in submission.url:
+        print("FFF submission found; title: " + submission.title + "; url: " + submission.url)
+        data = requests.get(submission.url).text
+        print("Fetched data: " + data)
+
+
+
 reddit = praw.Reddit(user_agent='fffbot/2.0 (by /u/fffbot; pyfff; PRAW)')
 
 subs = reddit.subreddit('bottesting+factorio')
 
 print("Starting to listen for submissions")
 for submission in subs.stream.submissions():
-    print("Encountered submission: " + submission.title + "; " + submission.url)
-    if 'factorio.com/blog/post/fff' in submission.url:
-        print("FFF submission found; title: " + submission.title + "; url: " + submission.url)
-        data = requests.get(submission.url).text
-        print("Fetched data: " + data)
+    process_submission(submission)
