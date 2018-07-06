36b5427651f01fbd178e8ebdd0be38af8061a786
fffbot <38205055+fffbot@users.noreply.github.com>
2018-07-06 22:14:02 +0200

    Add Markdown formatted comment to test post

--- fff.py
+++ fff.py
@@ -1,20 +1,65 @@
 import praw
 import requests
+import html2text
+import re
 
 
 def process_submission(submission):
-    print("Encountered submission: " + submission.title + "; " + submission.url)
+    print("Encountered submission; id: " + submission.id + "; title: " + submission.title + "; url: " + submission.url)
     if 'factorio.com/blog/post/fff' in submission.url:
-        print("FFF submission found; title: " + submission.title + "; url: " + submission.url)
-        data = requests.get(submission.url).text
-        print("Fetched data: " + data)
+        print("Submission identified as FFF post")
+        if 'test' in submission.title:
+            print('Title contains "test"')
+            html = requests.get(submission.url).text
+            print("Fetched data: ", len(html))
+            clipped = clip(html)
+            markdown = to_markdown(clipped)
+            print('Output markdown:\n' + markdown + '\nLength: ', len(markdown))
+            comment = submission.reply(markdown[:9990] + ' (...)')
+            print('Added comment ' + comment.id)
 
 
+def clip(html):
+    h2_index = html.find('<h2')
+    if h2_index == -1:
+        print("No <h2 found in text: ", html)
+        return
 
-reddit = praw.Reddit(user_agent='fffbot/2.0 (by /u/fffbot; pyfff; PRAW)')
+    footer_index = html.find('"footer"', h2_index)
+    if footer_index == -1:
+        print('No "footer" found in text: ', html)
+        return
 
-subs = reddit.subreddit('bottesting+factorio')
+    div_index = html.rfind('<div', 0, footer_index)
+    if div_index == -1:
+        print('<div not found in text: ', html)
+        return
 
-print("Starting to listen for submissions")
-for submission in subs.stream.submissions():
-    process_submission(submission)
+    header_to_div = html[h2_index:div_index]
+    return header_to_div
+
+
+def to_markdown(html):
+    md = html2text.html2text(html)
+    images_to_urls = re.sub(r'!\[\]\((.+)\)', r'(\g<1>)', md)
+    return images_to_urls.replace(r'(/blog/)', r'(https://www.factorio.com/blog/)').strip()
+
+
+def do_eet():
+    reddit = praw.Reddit(user_agent='fffbot/2.0 (by /u/fffbot; pyfff; PRAW)')
+
+    subs = reddit.subreddit('bottesting+factorio')
+
+    print("Starting to listen for submissions")
+    print("Skipping first 100 submissions")
+    i = 1
+    for submission in subs.stream.submissions():
+        if i > 100:
+            process_submission(submission)
+        else:
+            print("Skipping submission #" + str(i) + ": " + submission.id + " (" + submission.title + ")")
+            i = i + 1
+
+
+if __name__ == '__main__':
+    do_eet()
