33fe6187cfd8d964b304cb4248dc69c721f9d573
fffbot <38205055+fffbot@users.noreply.github.com>
2019-01-31 21:30:43 +0100

    use git shorthash as version

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 GIT_HASH=`git rev-parse --verify HEAD`
 BUILD_DATE=`date --iso-8601=seconds`
+
+
+SHORT_HASH=$(git rev-parse --short HEAD)
+IS_DIRTY=$(git diff --quiet && echo 0 || echo 1)
+
+
+SUFFIX=
+
+if [[ ${IS_DIRTY} -eq 1 ]]; then
+  echo Dirty workdir, using _d suffix. Git status:
+  git status -s
+  SUFFIX=_d
+else
+  echo Workdir is clean
+fi
+
+TAG_NAME=${SHORT_HASH}${SUFFIX}
+
 
 docker build \
     --build-arg git_hash=${GIT_HASH} \
     --build-arg build_date=${BUILD_DATE} \
     --no-cache \
-    . -t fffbot
+    -t fffbot:${TAG_NAME} .
